6d86bd311c79be3d12ce3a23ac7111b37b0e1d91
amd61 <arlenem.mcdaid@googlemail.com>
2017-01-22 22:45:08 +0000

    updated'

--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -11,18 +11,48 @@ class TestRooms < MiniTest::Test
 
     @song1 = Songs.new("Cool Kids", "Echosmith")
     @song2 = Songs.new("Jungle Boogie", "Kool & The Gang")
-    @coolSongs = [@song1, @song2]
+    @song3 = Songs.new("Cool", "Alesso")
+    @cool_songs = [@song1, @song2]
 
-    @room = Rooms.new("Cool Room", @coolSongs, [])
+    
     @guest1 = Guests.new("Cool dude 1")
     @guest2 = Guests.new("Cool dude 2")
     @guest3 = Guests.new("Cool dude 3")
-
     @guests = [@guest1, @guest2, @guest3]
+
+    @room = Rooms.new("Cool Room", @cool_songs, @guests)
     end
 
     def test_room_name
      assert_equal("Cool Room", @room.name)
   end
 
-end 
+ def test_add_song_to_room
+    @room.add_song(@song3)
+    assert_equal(3, @room.songs.count)
+  end
+   
+   def test_next_song
+    
+    @room.next_song()   
+    assert_equal(1, @room.songs.count)
+    
+    end
+
+      def test_check_in_guest
+
+        @room.check_in_guest(@guest1)
+        assert_equal(4, @room.guests.count)
+      end 
+
+    def test_check_out_guest
+      
+      @room.check_out_guest(@guest1)
+      assert_equal(2, @room.guests.count)
+    
+    end
+
+end
+
+
+
